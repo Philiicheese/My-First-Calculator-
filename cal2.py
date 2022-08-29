3f1de04c314bd683d5f0f82ffa9b05d118eaa5cb
Philiicheese <102993115+Philiicheese@users.noreply.github.com>
2022-08-29 06:33:12 +0000

    Add files via upload

---
+++ cal2.py
@@ -0,0 +1,22 @@
+#A Different Calculator
+
+num1=input("insert a number:")
+variable=input("insert a variable(+,-,*,/):")
+num2=input("insert a number:")
+
+add=float(num1) + float(num2)
+subtract=float(num1) - float(num2)
+multiply=float(num1) * float(num2)
+divide=float(num1) / float(num2)
+
+if variable == "+":
+    print(add)
+elif variable == "-":
+    print(subtract)
+elif variable == "*":
+    print(multiply)
+elif variable == "/":
+    print(divide) 
+
+else:
+    print("Error")
